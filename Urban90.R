42e23dd6679436fc3dc3569735c2aadcb4a6a1bf
HamidehIraj <hamideh.iraj@rocketmail.com>
2014-02-14 23:54:38 +0330

    02-data munging
    data munging

--- Urban90.R
+++ Urban90.R
@@ -1,9 +1,14 @@
 #Try to increase java Heap size(sufficient), by using
 options(java.parameters = "-Xmx32g")
 library(xlsx)
-urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1)
+urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1,colClasses=)
 
 #Try to increase java Heap size(sufficient), by using
 #options(java.parameters = "-Xmx32g")
 #library(XLConnect)
 #urban <- readWorksheetFromFile("SumU90.xlsx", sheet=1)
+
+##Data Munging
+tolower(names(urban))
+str(urban)
+nlevels(urban$ADDRESS)
