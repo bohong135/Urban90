57b4332939805493a8cf635239eadcd8f2f6d5c4
HamidehIraj <hamideh.iraj@rocketmail.com>
2014-02-14 23:33:10 +0330

    01-reading excel file
    reading excel file

--- Urban90.R
+++ Urban90.R
@@ -1,1 +1,9 @@
+#Try to increase java Heap size(sufficient), by using
+options(java.parameters = "-Xmx32g")
 library(xlsx)
+urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1)
+
+#Try to increase java Heap size(sufficient), by using
+#options(java.parameters = "-Xmx32g")
+#library(XLConnect)
+#urban <- readWorksheetFromFile("SumU90.xlsx", sheet=1)
