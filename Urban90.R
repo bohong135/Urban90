3078629b1c1c9e93fceda3560a9dbcea1a495f24
HamidehIraj <hamideh.iraj@rocketmail.com>
2014-02-18 23:44:12 +0330

    2014-02-18 data munging part 2
    2014-02-18 data munging part 2

--- Urban90.R
+++ Urban90.R
@@ -1,14 +1,42 @@
+## loading libraries
+
 #Try to increase java Heap size(sufficient), by using
 options(java.parameters = "-Xmx32g")
 library(xlsx)
-urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1,colClasses=)
+--------------------------------------------------------------
+## loading data
+urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1)
 
 #Try to increase java Heap size(sufficient), by using
 #options(java.parameters = "-Xmx32g")
 #library(XLConnect)
 #urban <- readWorksheetFromFile("SumU90.xlsx", sheet=1)
-
+-------------------------------------------------------------
 ##Data Munging
-tolower(names(urban))
+# to ease calling variables we rename them to lower case
+names(urban) <- tolower(names(urban))
 str(urban)
-nlevels(urban$ADDRESS)
+--------------------------------
+#creating a backup
+urbanbackup <- urban
+--------------------------------
+# using regular expression to find appropriate variables for converting factor to numeric
+dpattern = '*?daramad*'
+dindex = grep(dpattern, names(urban))
+dindex
+#names(urban)[dindex]
+
+for (i in dindex) {
+    urban[ ,i] <-as.numeric(urban[ ,i])
+}
+
+#class(urban$daramad03)
+--------------------------------
+# adding hazine to numeric
+hvector <- c("nhazineh","ghazineh","vkhorak","vgheirkhorak")
+for (j in hvector){
+  hindex<- which(names(urban)==j)
+  urban[ ,hindex] <-as.numeric(urban[ ,hindex])
+}
+
+#class(urban$nhazineh)
