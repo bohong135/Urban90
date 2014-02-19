e2e49fa01ecba2b98ff807da5ed6a894eca010fe
HamidehIraj <hamideh.iraj@rocketmail.com>
2014-02-19 12:44:20 +0330

    2014-02-18 data munging part 2- correction
    2014-02-18 data munging part 2- correction

--- Urban90.R
+++ Urban90.R
@@ -1,25 +1,25 @@
-## loading libraries
+# loading libraries
 
 #Try to increase java Heap size(sufficient), by using
 options(java.parameters = "-Xmx32g")
 library(xlsx)
---------------------------------------------------------------
-## loading data
+#--------------------------------------------------------------
+# loading data
 urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1)
 
 #Try to increase java Heap size(sufficient), by using
 #options(java.parameters = "-Xmx32g")
 #library(XLConnect)
 #urban <- readWorksheetFromFile("SumU90.xlsx", sheet=1)
--------------------------------------------------------------
-##Data Munging
+#-------------------------------------------------------------
+#Data Munging
 # to ease calling variables we rename them to lower case
 names(urban) <- tolower(names(urban))
 str(urban)
---------------------------------
+#--------------------------------
 #creating a backup
 urbanbackup <- urban
---------------------------------
+#--------------------------------
 # using regular expression to find appropriate variables for converting factor to numeric
 dpattern = '*?daramad*'
 dindex = grep(dpattern, names(urban))
@@ -31,12 +31,15 @@ for (i in dindex) {
 }
 
 #class(urban$daramad03)
---------------------------------
+#--------------------------------
 # adding hazine to numeric
 hvector <- c("nhazineh","ghazineh","vkhorak","vgheirkhorak")
-for (j in hvector){
+for (j in hvector){s
   hindex<- which(names(urban)==j)
   urban[ ,hindex] <-as.numeric(urban[ ,hindex])
 }
 
 #class(urban$nhazineh)
+#---------------------------------
+# Data Exploration
+summary(urban$daramad)
