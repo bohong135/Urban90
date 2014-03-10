1ee9dcb1e184f827fe7c59cb75fb4667da709912
Hamideh Iraj <hamideh.iraj@rocketmail.com>
2014-03-10 23:12:21 +0330

    2014-03-10 continuing exploratory analysis-join
    2014-03-10 continuing exploratory analysis-join

--- Urban90.R
+++ Urban90.R
@@ -3,10 +3,15 @@
 #Try to increase java Heap size(sufficient), by using
 options(java.parameters = "-Xmx32g")
 library(xlsx)
+library(sqldf)
 #--------------------------------------------------------------
 # loading data - 100 rows
 if(!(exists("urban"))){
   urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1,endrow=100)
+}
+# loading data for join
+if(!(exists("joinsheet"))){
+  joinsheet <- read.xlsx2("SumU90.xlsx",sheetIndex=2)
 }
 #urban <- read.xlsx2("SumU90.xlsx",sheetIndex=1)
 
@@ -72,9 +77,9 @@ urban2 <- urban1[ ,!(names(urban)) %in% hvector]
 #head(treeurbanf$daramad)
 #----------------------------------------------------------
 #exploratory data analysis
-plot(daramad ~ a01, data=urban2,varwidth = TRUE,col = rainbow(2)) #probably 1-man 2-woman
-plot(daramad ~ a02, data=urban2,varwidth = TRUE,range = 0,col = rainbow(20))
-plot(daramad ~ a03, data=urban2,varwidth = TRUE,col = rainbow(2)) #?
+plot(daramad ~ a01, data=urbantest,varwidth = TRUE,col = rainbow(2)) # 1-man 2-woman
+plot(daramad ~ a02, data=urban2,varwidth = TRUE,range = 0,col = rainbow(20)) #age
+plot(daramad ~ a03, data=urban2,varwidth = TRUE,col = rainbow(2)) #1 literate 2 illiterate
 plot(daramad ~ a04, data=urban2,varwidth = TRUE,col = rainbow(3)) #?
 plot(daramad ~ a05, data=urban2,varwidth = TRUE,col = rainbow(3)) #?
 plot(daramad ~ a06, data=urban2,varwidth = TRUE,col = rainbow(30)) #?
@@ -84,12 +89,30 @@ plot(daramad ~ a09, data=urban2,varwidth = TRUE,col = rainbow(30)) #?
 plot(daramad ~ a10, data=urban2,varwidth = TRUE,col = rainbow(30)) #?
 plot(daramad ~ a10new, data=urban2,varwidth = TRUE,col = rainbow(30)) #?
 
+urbantest <- urban2
 
 
+#--correct
+levels(urbantest$a01)[levels(urbantest$a01)== "1" ] <- "man"
+levels(urbantest$a01)[levels(urbantest$a01)== "2" ] <- "woman"
 
+summary(urbantest$a01)
+unique(urbantest$a01)
 
-
-
+#------------------------
+# for  (i in head(names(urbantest))) {
+# # urbantest<-  merge(x = urbantest, y = joinsheet, by = "CustomerId", all = TRUE)
+#   sqldf('update urbantest 
+#         set urbantest$i=
+#          from urbantest ,joinsheet
+# 
+# 
+#         where i==field and 
+#         where age is not null group by age')
+#   
+#   
+# }
+    
 
 
 #Prototyping data modeling
